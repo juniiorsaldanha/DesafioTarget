73e0e26c592c1e47b3d2caf64753aab0fbc5d7a7
Junior Saldanha <saldanhaplay@hotmail.com>
2022-08-01 21:17:18 -0300

    first commit

---
+++ Desafio2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        //True se o número é um quadrado perfeito
+        static bool QuadradoPerfeito(int x)
+        {
+            int valor = (int)Math.Sqrt(x); 
+            if(valor * valor == x)
+                return true; 
+            else
+                return false; 
+        }
+        //Regra matemática que: se 5*n*n + 4 ou 5*n*n - 4 for um quadrado perfeito
+        //então n é um número de fibonnaci
+        static bool Fibonnaci(int n)
+        {
+            if(QuadradoPerfeito(5 * n * n + 4) || QuadradoPerfeito(5 * n * n - 4))
+                return true; 
+            else
+                return false; 
+        }
+        static void Main(String[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine(Fibonnaci(x) ? x + " É um numero Fibonnaci" : x + " Não é um numero Fibonnaci"); 
+        }
+    }
+}
---
+++ Desafio5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void reverso(string s)
+        {
+            string aux = "";
+            char letra;  
+            for(int i = 0; i < s.Length; i++)
+            {
+                letra = s[i]; 
+                aux = letra + aux; 
+            }
+
+            Console.WriteLine($"Palavra invertida: " + aux); 
+        }
+        static void Main(String[] args)
+        {
+            string s = Console.ReadLine(); 
+
+            Console.WriteLine($"Palavra: {s}"); 
+
+            reverso(s); 
+        }
+    }
+}
